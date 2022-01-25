9b8c2ed0c547b3fd2f15fb2a01fc9437910d5ae3
heyar341 <kityonya@gmail.com>
2022-01-26 00:29:09 +0900

    引数の型が間違っていたので修正

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,6 @@ from hashlib import sha256
 from os import environ
 
 
-def hash_password(password: str):
-    salt = environ["SALT"]
-    return sha256(password + salt)
+def hash_password(password: str) -> hex:
+    salt = environ.get("SALT")
+    return sha256((password + salt).encode()).hexdigest()
