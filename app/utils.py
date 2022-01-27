1dacbe551dde5ddb615dd21cc757faaa699a5a20
heyar341 <kityonya@gmail.com>
2022-01-27 16:50:15 +0900

    パスワードチェック関数追加

--- app/utils.py
+++ app/utils.py
@@ -5,3 +5,11 @@ from os import environ
 def hash_password(password: str) -> hex:
     salt = environ.get("SALT")
     return sha256((password + salt).encode()).hexdigest()
+
+
+def compare_hash(raw_password: str, hashed_password: str) -> bool:
+    hashed = hash_password(raw_password)
+    if hashed == hashed_password:
+        return True
+    else:
+        return False
