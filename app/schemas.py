688fb5718ba346557bebb2425e87ecf4a6d4c8a0
heyar341 <kityonya@gmail.com>
2022-01-27 16:49:19 +0900

    ユーザー情報更新用schemaを定義

--- app/schemas.py
+++ app/schemas.py
@@ -6,3 +6,18 @@ class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+
+class NameUpdate(BaseModel):
+    current_username: str
+    new_username: str
+
+
+class EmailUpdate(BaseModel):
+    current_email: EmailStr
+    new_email: EmailStr
+
+
+class PasswordUpdate(BaseModel):
+    current_password: str
+    new_password: str
