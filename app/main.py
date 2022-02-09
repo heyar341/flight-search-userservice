00d07589bd7019f636592f7dd89078ee2b330e12
heyar341 <kityonya@gmail.com>
2022-02-09 14:58:14 +0900

    SQLAlchemyの仕様に合わせ、models.pyを絶対パスでimportするよう修正

--- app/main.py
+++ app/main.py
@@ -9,13 +9,16 @@ from sqlalchemy.orm import Session
 from schemas import (UserCreateReq, UserOut, NameUpdate, EmailUpdate,
                      PasswordUpdate, LoginData)
 from utils import hash_password, compare_hash, check_token
-import models
+import app.models as models
 from database import get_db
 from rabbitmq import publish_message, ConsumerThread
 from access_token import create_access_token, verify_access_token
+from routers import update
 
 app = FastAPI()
 logger = getLogger("uvicorn")
+
+app.include_router(update.router)
 
 
 @app.post("/register", status_code=status.HTTP_201_CREATED)
@@ -61,113 +64,6 @@ def get_user(req: Request, db: Session = Depends(get_db)) -> UserOut:
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"ユーザー情報が取得できませんでした。")
     return user
-
-
-@app.patch("/update/username", status_code=status.HTTP_200_OK)
-def update_username(
-        update_data: NameUpdate,
-        req: Request,
-        db: Session = Depends(get_db)) -> HTTPException or None:
-    access_token = req.cookies.get("access_token")
-    if not access_token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="ログインしていません。")
-    token_data, error = verify_access_token(access_token)
-    if error:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=error)
-    db_query = db.query(models.User).filter(
-        models.User.username == update_data.current_username,
-        models.User.id == token_data.user_id)
-    user_exists = db_query.first()
-    if not user_exists:
-        logger.warning(
-            f"Invalid update request came to /update/username . Request's "
-            f"current_username was {update_data.current_username}, but didn't "
-            f"matched user:{token_data.user_id}'s username.")
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=f"不正なユーザー情報が送信されました。")
-    db_query.update(
-        {models.User.username: update_data.new_username,
-         models.User.updated_at: datetime.datetime.now()},
-        synchronize_session=False)
-    db.commit()
-    logger.info(f"User id:{token_data.user_id} updated username")
-
-
-@app.patch("/update/email", status_code=status.HTTP_200_OK)
-def update_email(
-        update_data: EmailUpdate,
-        req: Request,
-        db: Session = Depends(get_db)) -> None:
-    access_token = req.cookies.get("access_token")
-    if not access_token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="ログインしていません。")
-    token_data, error = verify_access_token(access_token)
-    if error:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=error)
-    db_query = db.query(models.User).filter(
-        models.User.email == update_data.current_email,
-        models.User.id == token_data.user_id)
-    user_exists = db_query.first()
-    if not user_exists:
-        logger.warning(
-            f"Invalid update request came to /email/{token_data.user_id}. "
-            f"Request's current_email was {update_data.current_email}, "
-            f"but didn't matched user:{token_data.user_id}'s email.")
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=f"不正なユーザー情報が送信されました。")
-    email_exists = db.query(models.User.email).filter(
-        models.User.email == update_data.new_email).first()
-    if email_exists:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=f"{update_data.new_email} はすでに登録されています。")
-
-    db_query.update({models.User.email: update_data.new_email,
-                     models.User.updated_at: datetime.datetime.now()},
-                    synchronize_session=False)
-    db.commit()
-    publish_message(message={"email": update_data.new_email},
-                    queue_name="update_email_email")
-    logger.info(f"User id:{token_data.user_id} updated email")
-
-
-@app.patch("/update/password", status_code=status.HTTP_200_OK)
-def update_password(
-        update_data: PasswordUpdate,
-        req: Request,
-        db: Session = Depends(get_db)) -> None:
-    access_token = req.cookies.get("access_token")
-    if not access_token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="ログインしていません。")
-    token_data, error = verify_access_token(access_token)
-    if error:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=error)
-    db_query = db.query(models.User).filter(
-        models.User.id == token_data.user_id)
-    user = db_query.first()
-    password_matched = compare_hash(
-        raw_password=update_data.current_password,
-        hashed_password=user.password)
-    if not password_matched:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="入力されたパスワードが間違っています。")
-    db_query.update(
-        {models.User.password: hash_password(update_data.new_password),
-         models.User.updated_at: datetime.datetime.now()},
-        synchronize_session=False)
-    db.commit()
-    logger.info(f"User id:{token_data.user_id} updated password")
 
 
 @app.post("/login", status_code=status.HTTP_200_OK)
